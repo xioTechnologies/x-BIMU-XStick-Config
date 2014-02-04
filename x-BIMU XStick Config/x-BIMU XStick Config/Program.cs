b10fffe1b9b3c305dcaa400f03b5aab7efc3a8c8
xioTechnologies <social@x-io.co.uk>
2014-02-04 23:37:02 +0000

    Repeat on key press

--- x-BIMU XStick Config/x-BIMU XStick Config/Program.cs
+++ x-BIMU XStick Config/x-BIMU XStick Config/Program.cs
@@ -13,48 +13,45 @@ namespace XStick_Config
         static void Main(string[] args)
         {
             Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Name + " " + Assembly.GetExecutingAssembly().GetName().Version.Major.ToString() + "." + Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString());
-            List<string> configuredPortNames = new List<string>();
-            while (true)
+            do
             {
                 foreach (string portName in SerialPort.GetPortNames())
                 {
-                    if (!configuredPortNames.Contains(portName))
+                    Console.Write("Configuring XStick on " + portName + "...");
+                    try
                     {
-                        Console.Write("Configuring XStick on " + portName + "...");
-                        try
-                        {
-                            SerialPort serialPort = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
-                            //serialPort.Handshake = Handshake.RequestToSend;
-                            //serialPort.DtrEnable = true;
-                            serialPort.Open();
-                            Thread.Sleep(1100);
-                            serialPort.Write("+++");    // enter command mode
-                            Thread.Sleep(1100);
-                            serialPort.Write("ATRE\r");  // restore defaults
-                            Thread.Sleep(100);
-                            serialPort.Write("ATDLFFFF\r"); // destination address = 0xFFFF (broadcast to all addresses)
-                            Thread.Sleep(100);
-                            serialPort.Write("ATBD7\r");  // Baud = 115200
-                            Thread.Sleep(100);
-                            serialPort.Write("ATD55\r");  // D5 = output high
-                            Thread.Sleep(100);
-                            serialPort.Write("ATGT64\r");  // guard time = 200 ms
-                            Thread.Sleep(100);
-                            serialPort.Write("ATWR\r");   // save settings
-                            Thread.Sleep(100);
-                            serialPort.Write("ATFR\r");   // software reset
-                            serialPort.Close();
-                            Console.WriteLine("Compelte.");
-                            configuredPortNames.Add(portName);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine("Failed (" + e.Message + ")");
-                            Thread.Sleep(1000);
-                        }
+                        SerialPort serialPort = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+                        //serialPort.Handshake = Handshake.RequestToSend;
+                        //serialPort.DtrEnable = true;
+                        serialPort.Open();
+                        Thread.Sleep(1100);
+                        serialPort.Write("+++");    // enter command mode
+                        Thread.Sleep(1100);
+                        serialPort.Write("ATRE\r");  // restore defaults
+                        Thread.Sleep(100);
+                        serialPort.Write("ATDLFFFF\r"); // destination address = 0xFFFF (broadcast to all addresses)
+                        Thread.Sleep(100);
+                        serialPort.Write("ATBD7\r");  // Baud = 115200
+                        Thread.Sleep(100);
+                        serialPort.Write("ATD55\r");  // D5 = output high
+                        Thread.Sleep(100);
+                        serialPort.Write("ATGT64\r");  // guard time = 200 ms
+                        Thread.Sleep(100);
+                        serialPort.Write("ATWR\r");   // save settings
+                        Thread.Sleep(100);
+                        serialPort.Write("ATFR\r");   // software reset
+                        serialPort.Close();
+                        Console.WriteLine("Compelte.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Failed (" + e.Message + ")");
+                        Thread.Sleep(1000);
                     }
                 }
+                Console.WriteLine("Press Esc to exit or any other key to repeat...");
             }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
